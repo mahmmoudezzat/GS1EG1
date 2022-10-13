aff09672372e128084a2453817bc09bf09d5d4d9
mohamxx <mohamxx@hotmail.com>
2022-10-13 07:50:25 +0200

    format fix

--- ChangeMobileFormat/change_mobile_format/models/oneExcute.py
+++ ChangeMobileFormat/change_mobile_format/models/oneExcute.py
@@ -17,13 +17,15 @@ class one_Excute_phone(models.Model):
             if index != -1 and index != 0 and index != 1:
                 phone_number = phone_number[:index + 1]
             clean_phone_number = re.sub('[^0-9]+', '', phone_number)
-            if (clean_phone_number[0] == '+' and int(clean_phone_number[1]) == 2) or (
-                    int(clean_phone_number[0]) == 0 and int(clean_phone_number[1]) == 1) or (
-                    int(clean_phone_number[0]) == 0 and int(clean_phone_number[1]) == 0 and int(
-                clean_phone_number[2]) == 1):
-                clean_phone_number = clean_phone_number[:11]
-            else:
-                clean_phone_number = clean_phone_number[:15]
+            if len(clean_phone_number) < 10:
+                if (clean_phone_number[0] == '+' and int(clean_phone_number[1]) == 2) or (
+                        int(clean_phone_number[0]) == 0 and int(clean_phone_number[1]) == 1) or (
+                        int(clean_phone_number[0]) == 0 and int(clean_phone_number[1]) == 0 and int(
+                    clean_phone_number[2]) == 1):
+                    clean_phone_number = clean_phone_number[:11]
+                else:
+                    clean_phone_number = clean_phone_number[:15]
+            
             if int(clean_phone_number[0]) == 0 and int(clean_phone_number[1]) == 0 and int(clean_phone_number[2]) == 1:
                 tmp = list(clean_phone_number)
                 tmp[0] = '+'
