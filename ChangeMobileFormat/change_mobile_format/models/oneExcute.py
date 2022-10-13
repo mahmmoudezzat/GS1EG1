f1920f071ebd698337c251124b655573ab1b6a35
mohamxx <mohamxx@hotmail.com>
2022-10-13 08:18:36 +0200

    fix

--- ChangeMobileFormat/change_mobile_format/models/oneExcute.py
+++ ChangeMobileFormat/change_mobile_format/models/oneExcute.py
@@ -18,6 +18,8 @@ class one_Excute_phone(models.Model):
             if index != -1 and index != 0 and index != 1:
                 phone_number = phone_number[:index + 1]
             clean_phone_number = re.sub('[^0-9]+', '', phone_number)
+            if len(clean_phone_number) < 8:
+                return clean_phone_number
             if len(clean_phone_number) >= 10:
                 if (clean_phone_number[0] == '+' and int(clean_phone_number[1]) == 2) or (
                         int(clean_phone_number[0]) == 0 and int(clean_phone_number[1]) == 1) or (
